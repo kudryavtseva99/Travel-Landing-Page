e351bfa21b485f2533b4d2009eff57c112e90a7f
kudryavtsevaa99 <kudryyaaa@yandex.ru>
2023-09-22 22:41:46 +0600

    Fix img urls + js refacto

--- main.js
+++ main.js
@@ -35,40 +35,36 @@
 
 
 // Scroll to anchors
+const ease = function (t, b, c ,d) {
+    t /= d / 2;
+    if (t < 1) return c / 2 * t * t + b;
+    t--;
+    return -c / 2 * (t *(t - 2) - 1) + b;
+};
 
 (function () {
-    const smoothScroll = function (targetEl, duration) {
+    const smoothScroll = function (targetName, duration) {
         const headerElHeight = document.querySelector('.header').clientHeight;
-        let target = document.querySelector(targetEl);
-        let targetPosition = target.getBoundingClientRect().top - headerElHeight;
+        let targetElement = document.querySelector(targetName);
+        let targetPosition = targetElement.getBoundingClientRect().top - headerElHeight;
         let startPosition = window.pageYOffset;
         let startTime = null;
 
-        const ease = function (t, b, c ,d) {
-            t /= d / 2;
-            if (t < 1) return c / 2 * t * t + b;
-            t --;
-            return -c / 2 * (t *(t - 2) - 1) + b;
-        };
-
         const animation = function(currentTime) {
             if(startTime === null) startTime = currentTime;
-            const timeElapsed = currentTime -startTime;
-            const run = ease(timeElapsed, startPosition, targetPosition, duration);
-            window.scrollTo(0, run);
+            const timeElapsed = currentTime - startTime;
+            const nextYCoordinate = ease(timeElapsed, startPosition, targetPosition, duration);
+            window.scrollTo(0, nextYCoordinate);
             if(timeElapsed < duration) requestAnimationFrame(animation);
         };
         requestAnimationFrame(animation);
     };
 
-    const scrollTo = function () {
-        const links = document.querySelectorAll('.js-scroll');
-        links.forEach(each => {
-            each.addEventListener('click', function () {
-                const currentTarget = this.getAttribute('href');
-                smoothScroll(currentTarget, 1000);
-            });
+    const links = document.querySelectorAll('.js-scroll');
+    links.forEach(each => {
+        each.addEventListener('click', function () {
+            const target = this.getAttribute('href');
+            smoothScroll(target, 1000);
         });
-    };
-    scrollTo();
+    });
 }());
